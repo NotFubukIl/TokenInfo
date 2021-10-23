aeacb20087423d93768e66445a1b5d27af335fed
GayarraFrost <75570490+GayarraFrost@users.noreply.github.com>
2021-10-23 22:59:58 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import requests
+from colorama import Fore
+from os import *
+
+print(Fore.CYAN, """""")
+token = input(Fore.GREEN + "Insert A Token: ")
+
+req = requests.get("https://discord.com/api/v9/users/@me", headers = {"authorization": token})
+
+user = req.json()
+
+if not 'message' in user:
+    print(Fore.BLUE, f"""
+[USERNAME]: {user["username"]}#{user["discriminator"]}
+[ID]: {user["id"]}
+[AVATAR URL]: https://cdn.discordapp.com/avatars/{user["id"]}/{user["avatar"]}
+[Public FLAGS]: {user["public_flags"]}
+[EMAIL]: {user["email"]}
+[Phone ?]: {user["phone"]}
+[Biographie]: {user["bio"]}
+[Token]: {token}""", Fore.WHITE)
+else: print(Fore.RED,"INVALID TOKEN", Fore.WHITE)
