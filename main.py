13d68a3bb40beae9e049a420186725fd694b4f7c
Not Bouki <75570490+NotFubukIl@users.noreply.github.com>
2022-09-28 14:18:36 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,21 +1,49 @@
 import requests
-from colorama import Fore
-from os import *
-token = input(Fore.GREEN + "Insert A Token: ")
+from pystyle import Colors, Colorate
 
-req = requests.get("https://discord.com/api/v9/users/@me", headers = {"authorization": token})
+class Allah:
+    def main():
+        token = input("> Token: ")
+        al = Allah.getMainInfos(token)
+        if al == False: return print(Colorate.Horizontal(Colors.yellow_to_red, f"Token Isn't Valid", 1))
+        Settings = Allah.GetSettingsInfos(token)
+        Connections = Allah.GetConnections(token)
+        kdo = Allah.GetKdo(token)
+        for p, f in al.items(): 
+            if not f: f = None
+            print(Colorate.Horizontal(Colors.yellow_to_red, f"[{p}]: {f}", 1))
+        print(" ")  
+        for p, f in Settings.items(): 
+            if not f: f = None
+            print(Colorate.Horizontal(Colors.yellow_to_red, f"[{p}]: {f}", 1))
+        print(" ") 
+        for p in Connections: 
+            if not f: f = None
+            for n, z in p.items():
+                print(Colorate.Horizontal(Colors.yellow_to_red, f"[{n}]: {z}", 1))
+        print(" ")
+        for p in kdo: 
+            if not f: f = None
+            for n, z in p.items():
+                print(Colorate.Horizontal(Colors.yellow_to_red, f"[{n}]: {z}", 1))
+        print(" ")
+    def getMainInfos(token):
+        j = requests.get("https://discord.com/api/v9/users/@me", headers = {"authorization": token}).json()
+        if "message" in j: return False
+        else: return j
+    def GetSettingsInfos(token):
+        j = requests.get("https://discord.com/api/v9/users/@me/settings", headers = {"authorization": token}).json()
+        if "message" in j: return False
+        else: return j
+    def GetConnections(token):
+        j = requests.get("https://discordapp.com/api/v9/users/@me/connections", headers = {"authorization": token}).json()
+        if "message" in j: return False
+        else: return j
+    def GetKdo(token):
+        j = requests.get("https://discord.com/api/v8/users/@me/entitlements/gifts", headers = {"authorization": token}).json()
+        if "message" in j: return False
+        else: return j
 
-user = req.json()
-
-if not 'message' in user:
-    print(Fore.BLUE, f"""
-[USERNAME]: {user["username"]}#{user["discriminator"]}
-[ID]: {user["id"]}
-[AVATAR URL]: https://cdn.discordapp.com/avatars/{user["id"]}/{user["avatar"]}
-[Public FLAGS]: {user["public_flags"]}
-[EMAIL]: {user["email"]}
-[Phone ?]: {user["phone"]}
-[Biographie]: {user["bio"]}
-[Token]: {token}
-[By Not.Fubukii]""", Fore.WHITE)
-else: print(Fore.RED,"INVALID TOKEN", Fore.WHITE)
+if __name__ == "__main__": 
+    Allah.main()
+    input("")
---
+++ readme.md
@@ -0,0 +1,1 @@
+# Allah Token Info ! !! ! 
